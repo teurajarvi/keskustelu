010e5530f8fdcbcb0061d12160839cddd8694eb6
Jari-Pekka Teurajärvi <teurajarvi@hotmail.com>
2016-07-26 13:45:24 +0300

    Create keskustelu.h
    initial .h header file for program that prints a conversation and asks few questions (language Finnish)

---
+++ keskustelu.h
@@ -0,0 +1,13 @@
+/* oma header: keskustelu.h */
+#include <stdbool.h> /* C doesn't have build in boolean */
+
+/* muuttujat */
+#define FALSE 0
+#define TRUE 1
+
+/* tyypit */
+#define LUKUVIRHE 9999
+
+/* funktion määritelmät */
+void kysy_jotain( );
+bool tarkista_bye( );
